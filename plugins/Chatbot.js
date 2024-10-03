19d3a1ddde4f457fa77a10bee1d9b66025417cc0
Mohnd32145 <mohndalcot@gmail.com>
2024-10-03 07:59:36 +0300

    Update Chatbot.js

--- plugins/Chatbot.js
+++ plugins/Chatbot.js
@@ -3,9 +3,9 @@ let handler = m => m;
  handler.all = async function (m) { 
  let chat = global.db.data.chats[m.chat]; 
  let responses; 
- if (/^ميجو$/i.test(m.text)) { 
+ if (/^تيربو$/i.test(m.text)) { 
  responses = [ 
- 'تحت امرك حبي'
+ 'تحت امرك يقلبي'
  ]; 
  } else if (/^بوووت|بموووووووووت|بوت$/i.test(m.text)) { 
      responses = [ 
@@ -17,7 +17,7 @@ let handler = m => m;
      ];
  }else if (/^تست$/i.test(m.text)) { 
      responses = [ 
-       'شغال شغال تست',  
+       'شغال ولله يعم',  
      ];
  }
  if (responses) { 
